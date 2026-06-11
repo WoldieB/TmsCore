e0cd56d0679a6104694753fb2d9cdf6aaed2e690
woldieb <woldieb77@gmail.com>
2026-06-11 00:25:08 -0700

    update project

--- obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+054ad90438183bec32818168b374a5ca5081a883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93903882213a2a964a417abf3b8eaa9fe6f6d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
